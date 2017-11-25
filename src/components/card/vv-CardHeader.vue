248841fc6ea9f1cfbcd4950874cd22150b74a1aa
Thabo Titus <hello@thabotitus.co.za>
2017-11-25 13:18:29 +0200

    card header, content, footer

---
+++ src/components/card/vv-CardHeader.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="vv-card__header">
+    {{ title }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'vv-card-header',
+    props: {
+      title: '',
+    },
+  };
+</script>
--- src/containers/Library.vue
+++ src/containers/Library.vue
@@ -1,23 +1,52 @@
 <template>
   <div>
-    <h1>Library</h1>
-    <vv-button value="submit" class="vv-button--red" text="Click Me"></vv-button>
-    <vv-button value="submit" class="vv-button--round vv-button--primary vv-button--outline" text="Click Me"></vv-button>
-    <vv-button value="submit" class="vv-button--round vv-button--teal vv-button--large" text="Click Me Square"></vv-button>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1>Library</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col col--1/2-large">
+          <vv-card>
+            <vv-card-header title="My Cool Card"></vv-card-header>
+
+            <vv-card-content>
+                Now that there is the Tec-9, a crappy spray gun from South Miami.
+                This gun is advertised as the most popular gun in American crime.
+                Do you believe that shit? It actually says that in the little book
+                that comes with it: the most popular gun in American crime. Like
+                they're actually proud of that shit.
+            </vv-card-content>
+
+            <vv-card-footer>
+              <vv-button value="submit" class="vv-button--red vv-button--round" text="Magic"></vv-button>
+            </vv-card-footer>
+          </vv-card>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
   import vvButton from '../components/vv-Button';
+  import vvCard from '../components/card/vv-Card';
+  import vvCardHeader from '../components/card/vv-CardHeader';
+  import vvCardContent from '../components/card/vv-CardContent';
+  import vvCardFooter from '../components/card/vv-CardFooter';
 
   export default {
     name: 'Library',
     components: {
       vvButton,
+      vvCard,
+      vvCardHeader,
+      vvCardContent,
+      vvCardFooter,
     },
     data() {
       return {
-        msg: 'Welcome to Your Vue.js App',
       };
     },
   };
