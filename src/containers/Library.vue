4e2bff88841013347074493bb7a852fe3af9a40b
Thabo Titus <hello@thabotitus.co.za>
2017-11-25 16:56:08 +0200

    simple cards

--- src/containers/Library.vue
+++ src/containers/Library.vue
@@ -19,8 +19,30 @@
                 they're actually proud of that shit.
             </vv-card-content>
 
+            <vv-card-content>
+                Now that there is the Tec-9, a crappy spray gun from South Miami.
+                This gun is advertised as the most popular gun in American crime.
+                Do you believe that shit? It actually says that in the little book
+                that comes with it: the most popular gun in American crime. Like
+                they're actually proud of that shit.
+            </vv-card-content>
+
             <vv-card-footer>
-              <vv-button value="submit" class="vv-button--red vv-button--round" text="Magic"></vv-button>
+              <div class="row">
+                <div class="col col--1/2-medium col--1-small">
+                  <vv-button value="submit"
+                             class="vv-button--primary vv-button--round vv-button--fluid"
+                             text="Magic">
+                  </vv-button>
+                </div>
+
+                <div class="col col--1/2-medium col--1-small">
+                  <vv-button value="submit"
+                             class="vv-button--red vv-button--round vv-button--fluid"
+                             text="Delete"></vv-button>
+                </div>
+              </div>
+
             </vv-card-footer>
           </vv-card>
         </div>
